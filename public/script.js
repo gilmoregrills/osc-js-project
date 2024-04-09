d285c919f335a83febb1c88b189d34a73d62a5d7
Robin Lightfoot <robin.yonge@tractable.ai>
2024-04-09 11:59:47 +0100

    feat: log changes to opt in control channel

--- public/script.js
+++ public/script.js
@@ -6,6 +6,8 @@ console.log("OSC Port created");
 const handleControlCodes = (oscMsg) => {
   document.getElementById(`opt_${oscMsg.args[0]}`).textContent =
     `opt: ${oscMsg.args[1]}`;
+  document.getElementById("set_opt").textContent =
+    `set:channel:/${oscMsg.args[0]}.opt to: ${oscMsg.args[1]}`;
 };
 
 const renderRawInput = (oscMsg) => {
