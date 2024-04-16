42330b5e8da6610ba745a86020db7fbd4007b6c4
Robin Lightfoot <robin.yonge@tractable.ai>
2024-04-16 11:59:14 +0100

    feat: add sound for channels 1 and 2 (#5)
    * feat: remove now-defunct script.js
    
    * feat: add log, tidy up routing
    
    * feat: add button to start tone.js audio context
    
    * feat: reformat homepage
    
    * feat: add some initial sound generation, use user interaction to start audio context and open udp port so messages arent received early
    
    * feat: instantiate synths on receipt of message
    
    * feat: play sound out of channels 1/2
    
    * feat: document message spec
    
    * chore: remove file i added by accident lol

--- src/public/main.js
+++ src/public/main.js
@@ -8,26 +8,94 @@ var oscPort = new osc.WebSocketPort({
 });
 console.log(`OSC WebSocketPort created on ${wsUrl}`);
 
-oscPort.open();
-console.log("OSC WebSocketPort opened");
+document.getElementById("start")?.addEventListener("click", async () => {
+  console.log("Starting audio context.");
+  await Tone.start();
+  console.log("Audio context is ready.");
+  oscPort.open();
+  console.log("OSC WebSocketPort opened");
+});
 
-const renderRawInput = (oscMsg) => {
-  document.getElementById("input_receiver").textContent = `${JSON.stringify(
-    oscMsg,
-    null,
-    2,
-  )}_ routing args to channel_${oscMsg.address}`;
+const updateLastMessageForChannel = (channel, message) => {
+  document.getElementById(`channel_${channel}`).textContent = message;
+};
+
+var messageLog = [];
+const updateMessageLog = (oscMsg) => {
+  messageLog.push(JSON.stringify(oscMsg));
+  if (messageLog.length > 6) {
+    messageLog.shift();
+  }
+  const result = messageLog.map((val) => `<p>${val}</p>`).join("");
+  document.getElementById("log-messages").innerHTML = result;
+};
+
+const sendOsc = (channel, args) => {
+  console.log(
+    `sending osc from frontend to backend and back again on channel: ${channel} with args: ${args}`,
+  );
+  // todo: add validation that we're receiving only numbers
+  oscPort.send({
+    address: channel,
+    args: args.split(" ").map(Number),
+  });
+  document.getElementById("sent_message").textContent =
+    `sent: {address: ${channel}, args: ${args}}`;
+};
+
+const handleControlChannel = (oscMsg) => {
+  document.getElementById(`opt_${oscMsg.args[0]}`).textContent =
+    `opt: ${oscMsg.args[1]}`;
+  return `set:channel:/${oscMsg.args[0]}.opt to: ${oscMsg.args[1]}`;
+};
+
+const convertIntsToPitchOctave = (pitch, octave) => {
+  const pitchMap = {
+    1: "C",
+    2: "C#",
+    3: "D",
+    4: "D#",
+    5: "E",
+    6: "F",
+    7: "F#",
+    8: "G",
+    9: "G#",
+    10: "A",
+    11: "A#",
+    12: "B",
+  };
+  return `${pitchMap[pitch]}${octave}`;
+};
+
+const handleSynthChannel = (oscMsg) => {
+  const note = convertIntsToPitchOctave(oscMsg.args[0], oscMsg.args[1]);
+  const duration = Tone.Time(oscMsg.args[2] / 10).toNotation();
+
+  if (oscMsg.address === "/1") {
+    const oscSynth = new Tone.Synth().toDestination();
+    const now = Tone.now();
+    oscSynth.triggerAttackRelease(note, duration, now);
+  } else if (oscMsg.address === "/2") {
+    const membraneSynth = new Tone.MembraneSynth().toDestination();
+    const now = Tone.now();
+    membraneSynth.triggerAttackRelease(note, duration, now);
+  } else if (oscMsg.address === "/3") {
+    return "channel not yet implemented";
+  }
+
+  return `received: [${oscMsg.args}] played: ${note} for: ${duration}`;
 };
 
 // this is like our main function
 oscPort.on("message", (oscMsg) => {
-  console.log("OSC message received:", oscMsg);
-  renderRawInput(oscMsg);
-
+  console.log(`channel: ${oscMsg.address}, args: ${oscMsg.args}`);
+  var logMsg;
   if (oscMsg.address === "/0") {
-    handleControlCodes(oscMsg);
+    logMsg = handleControlChannel(oscMsg);
+  } else {
+    logMsg = handleSynthChannel(oscMsg);
   }
 
-  document.getElementById(`channel_${oscMsg.address}`).textContent =
-    `last_message: ${JSON.stringify(oscMsg.args)}`;
+  updateLastMessageForChannel(oscMsg.address, logMsg);
+  updateMessageLog(oscMsg);
 });
