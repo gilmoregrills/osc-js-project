7ded168ccd31ddef3b1b111e1053cfcd978aa5d2
Robin Lightfoot <robin.yonge@tractable.ai>
2024-04-26 11:10:37 +0100

    feat: save most recent control message for each option and load on refresh (#10)
    * feat: save control messages to dynamodb
    
    * feat: load control messages from db as part of initialisation
    
    * feat: bundle, and make it so NODE_ENV is production when deployed
    
    * fix: only send control messages to db, fix logging
    
    * fix: logging

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const WebSocket = require("ws");
 const os = require("os");
 const marked = require("marked");
 const readFileSync = require("fs").readFileSync;
+const {
+  DescribeTableCommand,
+  DynamoDBClient,
+} = require("@aws-sdk/client-dynamodb");
+const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
+const {
+  ScanCommand,
+  PutCommand,
+  DynamoDBDocumentClient,
+} = require("@aws-sdk/lib-dynamodb");
+const { fromSSO } = require("@aws-sdk/credential-provider-sso");
+const { fromInstanceMetadata } = require("@aws-sdk/credential-providers");
 
 // express
 const app = express();
@@ -49,6 +61,69 @@ app.post("/api/send-message", (req, res) => {
   );
 });
 
+var credentials;
+if (process.env.NODE_ENV == "production") {
+  credentials = fromInstanceMetadata()();
+} else {
+  credentials = fromSSO({ profile: "osc-chat" })();
+}
+
+const ddbClient = new DynamoDBClient({
+  region: "eu-west-2",
+  credentials: credentials,
+});
+const docClient = DynamoDBDocumentClient.from(ddbClient);
+const tableName = (async () => {
+  const ssmClient = new SSMClient({
+    region: "eu-west-2",
+    credentials: credentials,
+  });
+  const command = new GetParameterCommand({
+    Name: "/osc-chat/dynamodb-table-name",
+  });
+  const response = await ssmClient.send(command);
+  console.log(`Retrieved DynamoDB table name: ${response.Parameter.Value}`);
+  return response.Parameter.Value;
+})();
+
+const saveControlMessage = async (oscMsg) => {
+  console.log(
+    `Saving control message ${JSON.stringify(oscMsg)} to DynamoDB table: ${await tableName}`,
+  );
+
+  const command = new PutCommand({
+    TableName: await tableName,
+    Item: {
+      channelAndGroup: `${oscMsg.args[0]}${oscMsg.args[1]}`,
+      channel: oscMsg.address,
+      args: oscMsg.args,
+      timestamp: Date.now().toString(),
+    },
+  });
+
+  const response = await docClient.send(command);
+  return response;
+};
+
+app.get("/api/get-control-messages", async (req, res) => {
+  console.log(
+    `Fetching all control messages from DynamoDB table: ${await tableName}`,
+  );
+
+  const command = new ScanCommand({
+    TableName: await tableName,
+  });
+
+  const response = await docClient.send(command);
+  const messages = response.Items.map((item) => ({
+    address: item.channel,
+    args: item.args,
+  }));
+  console.log(`Retrieved control messages: ${JSON.stringify(messages)}`);
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ controlMessages: messages }));
+});
+
 const udpPort = new osc.UDPPort({
   localAddress: "0.0.0.0",
   localPort: 57121,
@@ -60,9 +135,12 @@ console.log("UDP port created on 0.0.0.0:57121");
 
 udpPort.on("message", (oscMsg, timeTag, info) => {
   console.log(
-    `Received OSC message via UDP: ${oscMsg}, relaying to WebSocket.`,
+    `Received OSC message via UDP: ${JSON.stringify(oscMsg)}, relaying to WebSocket.`,
   );
   console.log("Remote info is: .", info);
+  if (oscMsg.address === "/0") {
+    saveControlMessage(oscMsg);
+  }
 });
 
 udpPort.on("ready", () => {
@@ -92,7 +170,7 @@ wss.on("connection", (socket) => {
 
   socketPort.on("message", (oscMsg) => {
     console.log(
-      `Received OSC message via WebSocket: ${oscMsg}, redirecting it to UDP port.`,
+      `Received OSC message via WebSocket: ${JSON.stringify(oscMsg)}, redirecting it to UDP port.`,
     );
     udpPort.send(oscMsg, "0.0.0.0", "57121");
   });
