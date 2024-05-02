f60db73413922e2637131050b57214a2d9dfe906
Robin Lightfoot <robin.yonge@tractable.ai>
2024-05-02 11:13:37 +0100

    feat: add sequencer and remove the old input form (#13)
    * feat: working sequencer, very messy and ugly
    
    * feat: add visual feedback to sequencer
    
    * feat: tidy up, refactor, remove form
    
    * fix: small bugs, make more DRY

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,11 @@
-import { WebSocketPort } from "osc/dist/osc-browser";
-import { start } from "tone";
+import { start, getDestination, Transport } from "tone";
 import { messageLog, updateMessageLog } from "./logging";
 import { Channel, ControlChannel, allChannels } from "./channels";
+import { makeSequencer } from "./sequencer";
+import { WebSocketPort, timeTag } from "osc";
+import { messageStringToMessage } from "./utils";
 
+//setup websocket and OSC
 const wsUrl =
   location.host == "localhost:8080"
     ? "ws://localhost:8081"
@@ -13,36 +16,71 @@ var oscPort = new WebSocketPort({
 });
 console.log(`OSC WebSocketPort created on ${wsUrl}`);
 
-document
-  .getElementById("engine-start-button")
-  ?.addEventListener("click", async () => {
-    await start();
-    console.log("Audio context is ready");
-    oscPort.open();
-    console.log("OSC WebSocketPort opened");
-  });
-
-document
-  .getElementById("broadcast-form")
-  .addEventListener("submit", (event) => {
-    event.preventDefault();
-    const channel = `/${document.getElementById("channel").value}`;
-    const args = document.getElementById("args").value;
-    console.log(
-      `Sending osc from frontend to backend and back again on channel: ${channel} with args: ${args}`,
-    );
-    // todo: add validation that we're receiving only numbers
-    oscPort.send({
-      address: channel,
-      args: args.split(" ").map(Number),
-    });
-    document.getElementById("sent_message").textContent =
-      `sent: {address: ${channel}, args: ${args}}`;
-  });
-
-allChannels.initialise();
-
 oscPort.on("message", (oscMsg) => {
   allChannels.channels[oscMsg.address].handle(oscMsg);
   updateMessageLog(oscMsg);
 });
+
+//setup channels
+allChannels.initialise();
+
+//setup sequencer
+const grid = makeSequencer();
+var beat = 0;
+
+const startSequencerLoop = () => {
+  const repeat = (time) => {
+    const messages = [];
+    grid.forEach((row, index) => {
+      let item = row[beat];
+      let prevBeat = beat === 0 ? 7 : beat - 1;
+      document.getElementById(`${index}${prevBeat}`).style.textDecoration =
+        "none";
+      document.getElementById(`${index}${beat}`).style.textDecoration =
+        "underline";
+      if (item.isActive) {
+        const message = messageStringToMessage(
+          document.getElementById(`sequencer-message-field-${index}`).value,
+        );
+        messages.push(message);
+      }
+    });
+    if (messages.length > 0) {
+      oscPort.send({ packets: messages, timeTag: timeTag(time) });
+    }
+    beat = (beat + 1) % 8;
+  };
+
+  Transport.bpm.value = 60;
+  Transport.scheduleRepeat(repeat, "8n");
+};
+
+// setup event listeners/initialisers
+window.onload = () => {
+  document
+    .getElementById("engine-start-button")
+    ?.addEventListener("click", async () => {
+      await start();
+      console.log("Audio context is ready");
+      oscPort.open();
+      console.log("OSC WebSocketPort opened");
+      getDestination().volume.rampTo(-10, 0.001);
+      startSequencerLoop();
+      Transport.start();
+    });
+
+  document
+    .getElementById("broadcast-form")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+      const message = messageStringToMessage(
+        document.getElementById("text-input-message-field").value,
+      );
+      console.log(
+        `Sending osc from frontend to backend and back again: ${JSON.stringify(message)}`,
+      );
+      oscPort.send(message);
+      document.getElementById("sent-message").textContent =
+        `sent: ${JSON.stringify(message)}`;
+    });
+};
---
+++ src/client/sequencer.js
@@ -0,0 +1,101 @@
+class gridItem {
+  constructor(position, isActive) {
+    this.position = position;
+    this.isActive = isActive;
+    this.isHighlighted = false;
+  }
+
+  toggleActive() {
+    this.isActive = !this.isActive;
+  }
+
+  generateInnerHTML() {
+    var character = this.isActive ? "x" : ".";
+    return `
+      <p>${character}</p>
+    `;
+  }
+
+  render() {
+    const div = document.getElementById(this.position);
+    div.innerHTML = this.generateInnerHTML();
+  }
+
+  initialise(row) {
+    const div = document.createElement("div");
+    div.id = this.position;
+    div.class = "seqItem";
+    div.innerHTML = this.generateInnerHTML();
+    div.addEventListener("click", (e) => {
+      this.handleClick(e);
+    });
+    row.appendChild(div);
+  }
+
+  handleClick(e) {
+    this.toggleActive();
+    this.render();
+  }
+}
+
+const makeGrid = () => {
+  const rows = [];
+
+  for (let i = 0; i < 4; i++) {
+    const row = [];
+    var x = i;
+    for (let i = 0; i < 8; i++) {
+      var y = i;
+      row.push(new gridItem(`${x}${y}`, false));
+    }
+    rows.push(row);
+  }
+
+  return rows;
+};
+
+const getRandomInt = (min, max) => {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+};
+
+const generateMessageStringForInputFields = () => {
+  const channel = getRandomInt(1, 4);
+  const pitch = getRandomInt(1, 13);
+  const octave = getRandomInt(1, 8);
+  const length = 8;
+  return `${channel} ${pitch} ${octave} ${length}`;
+};
+
+export const makeSequencer = () => {
+  const grid = makeGrid();
+  var exampleMessages = [];
+  for (let i = 0; i < grid.length; i++) {
+    exampleMessages.push(generateMessageStringForInputFields());
+  }
+  const sequencer = document.getElementById("sequencer");
+  sequencer.innerHTML =
+    "<p>sequences messages that broadcast to all clients, try editing or check <a href='/spec/'>the message spec</a></p>";
+
+  // iterate through the grid
+  grid.forEach((row, rowIndex) => {
+    // create a parent div for each row
+    const seqRow = document.createElement("div");
+    seqRow.id = rowIndex;
+    seqRow.className = "sequencer-row";
+
+    // iterate through each note in the row
+    row.forEach((item, itemIndex) => {
+      // create a button for each note
+      item.initialise(seqRow);
+    });
+    const msgField = document.createElement("input");
+    msgField.type = "text";
+    msgField.value = exampleMessages[rowIndex];
+    msgField.id = `sequencer-message-field-${rowIndex}`;
+    seqRow.appendChild(msgField);
+    sequencer.appendChild(seqRow);
+  });
+  return grid;
+};
